1def63e553e0c28d7d9f1e8286ea4d29885d06da
Daniel Patrick Dicken <dpdicken@gmail.com>
2017-09-09 11:40:59 -0700

    update

--- Hello.java
+++ Hello.java
@@ -1,11 +1,31 @@
 public class Hello {
 
     public static void main(String[] args) {
+
+        goldLeader();
+        redLeader();
+        callSign();
+
+    }
+
+    public static void callSign() {
         
-        // Student 1 code
+        // Red Leader should add a print statement that says "Lock S-foils in attack position." 
+        // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
+    }
 
-        // student 2 code
+    public static void goldLeader() {
+
+        // Gold Leader should add a print statement that says "Loosen up!"
+    
+
+    }
+
+    public static void redLeader() {
+ 
+        // Red Leader should add a print statement that says "I copy, gold leader!"
+        
 
     }
 
