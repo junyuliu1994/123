8423de4a941e4763a2cec49d17af6f4fbe2a4eca
Feiran Yang <feiranyang@doberman.cs.arizona.edu>
2017-09-11 16:57:07 -0700

    hello.java added

--- Hello.java
+++ Hello.java
@@ -9,21 +9,22 @@ public class Hello {
     }
 
     public static void callSign() {
-        
-        // Red Leader should add a print statement that says "Lock S-foils in attack position." 
+        System.out.println("Lock S-foils in attack position.");
+        // Red Leader should add a print statement that says "Lock S-foils in attack position."
+        System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
 
     public static void goldLeader() {
-
+        System.out.println("Loosen up!");
         // Gold Leader should add a print statement that says "Loosen up!"
     
 
     }
 
     public static void redLeader() {
- 
+        System.out.println("I copy, gold leader!");
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
 
