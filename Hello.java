37bfef7ab2818b7dd8e5a99e1d95da0f0608e733
junyuliu1994 <junyuliu@email.arizona.edu>
2017-09-11 20:21:30 -0700

    asd

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-
+        System.out.println("Lock S-foils in attack position.")
         goldLeader();
         redLeader();
         callSign();
