d268684cea28885c25f15e1e6f7e6019db6f88da
junyuliu1994 <junyuliu@email.arizona.edu>
2017-09-11 19:28:26 -0700

    qweqwe

--- Hello.java
+++ Hello.java
@@ -9,13 +9,7 @@ public class Hello {
     }
 
     public static void callSign() {
-        
         System.out.println("Lock S-foils in attack position.");
-
-        System.out.println("qwe");
-
-        System.out.println("Lock S-foils in attack position.");
-
         // Red Leader should add a print statement that says "Lock S-foils in attack position."
         System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
@@ -24,14 +18,12 @@ public class Hello {
 
     public static void goldLeader() {
         System.out.println("Loosen up!");
-        System.out.println("Loosen up!");
         // Gold Leader should add a print statement that says "Loosen up!"
     
 
     }
 
     public static void redLeader() {
-        System.out.println("I copy, gold leader!");
         System.out.println("I copy, gold leader!");
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
