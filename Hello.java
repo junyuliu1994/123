507ced7098a30dbac10743e6cd861b9f03a3dd67
junyuliu1994 <junyuliu@email.arizona.edu>
2017-09-11 19:47:29 -0700

    qwe

--- Hello.java
+++ Hello.java
@@ -9,6 +9,7 @@ public class Hello {
     }
 
     public static void callSign() {
+        System.out.println("Lock S-foils in attack position.")
         // Red Leader should add a print statement that says "Lock S-foils in attack position."
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
