13888be40f2fcca4d70a7f00698a02df85c68ee7
junyuliu1994 <junyuliu@email.arizona.edu>
2017-09-11 20:48:49 -0700

    asd

--- Hello.java
+++ Hello.java
@@ -21,6 +21,7 @@ public class Hello {
 
     public static void goldLeader() {
         System.out.println("Loosen up!");
+        System.out.println("I copy, gold leader!");
         // Gold Leader should add a print statement that says "Loosen up!"
 
 
