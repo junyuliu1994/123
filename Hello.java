d77e31ab1c082d25a7b0af045c18d2fa068d0b7c
junyuliu1994 <junyuliu@email.arizona.edu>
2017-09-11 20:21:16 -0700

    qwe

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-
+        System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.")
         goldLeader();
         redLeader();
         callSign();
